f11f46048e07c58cc016df79d596e05249084f59
Anish Swarnakar <anishswarnakar57@gmail.com>
2025-11-14 12:22:55 +0530

    EDA and problem statement

--- src/exception.py
+++ src/exception.py
@@ -1,13 +1,29 @@
 import sys #Any exception that is been occured the sys library has that information
+import os
 
-def error_message_detail(error,error_detail:sys):#this is the function that tells me how my message should look int our file 
-    _,_,exc_tb=error_detail.exc_info()#the exc_tb has all the information related to the errors occured 
-    file_name=exc_tb.tb_frame.f_code.co_filename# custom exception for the python 
+# Add project root to Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from src.logger import logging
+
+# def error_message_detail(error,error_detail:sys):#this is the function that tells me how my message should look int our file 
+#     _,_,exc_tb=error_detail.exc_info()#the exc_tb has all the information related to the errors occured 
+#     file_name=exc_tb.tb_frame.f_code.co_filename# custom exception for the python 
+#     error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+#         file_name,exc_tb.tb_lineno,str(error))#to  get the line number along with the error message details 
+
+#     return error_message
+        
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
     error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
-        file_name,exc_tb.tb_lineno,str(error))#to  get the line number along with the error message details 
+     file_name,exc_tb.tb_lineno,str(error))
 
     return error_message
-        
+
+    
+
 
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
